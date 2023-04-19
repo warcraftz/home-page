40c1d60689f11692b82383c6f594fcb039a4c6f2
Mereles Fernando <118017313+warcraftz@users.noreply.github.com>
2023-04-19 19:38:06 +0100

    comit init

--- js/components/animated.components.js
+++ js/components/animated.components.js
@@ -3,19 +3,20 @@ $(document).ready(function (){
     var animated = `
     <div class="platform">
         <div class="cubes" id='cubes'>
-            <div class="face_01_ft border">H</div>
-            <div class="face_02_bc border">I</div>
-            <div class="face_03_rg border">!</div>
-            <div class="face_04_lf border">W</div>
-            <div class="face_05_tp border">A</div>
-            <div class="face_06_bo border">R</div>
-            <div class="face_07_ft border">C</div>
-            <div class="face_08_bc border">R</div>
-            <div class="face_09_rg border">A</div>
-            <div class="face_10_lf border">F</div>
-            <div class="face_11_tp border">T</div>
-            <div class="face_12_bo border">Z</div>
+            <div class="face_01_ft border"></div>
+            <div class="face_02_bc border"></div>
+            <div class="face_03_rg border"></div>
+            <div class="face_04_lf border"></div>
+            <div class="face_05_tp border"></div>
+            <div class="face_06_bo border"></div>
+            <div class="face_07_ft border"></div>
+            <div class="face_08_bc border"></div>
+            <div class="face_09_rg border"></div>
+            <div class="face_10_lf border"></div>
+            <div class="face_11_tp border"></div>
+            <div class="face_12_bo border"></div>
         </div>
+        <div class="base"></div>
     </div>
     `
     var content = $(document.querySelector('.animated .container'))
